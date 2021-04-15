2bd83d337a48ec759153cdd58f80d26f7169391e
igotodev <gotodev@protonmail.com>
2021-04-15 17:12:10 +0300

    Update tg bot

--- tg/main.go
+++ tg/main.go
@@ -1,91 +1,101 @@
+// Feed Telegram Bot 
+// https://habr.com/ru/rss/best/daily/?fl=ru              - лучшее за день по всем темам
+// https://www.opennet.ru/opennews/opennews_all_utf.rss   - rss от opennet
+
 package main
 
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"log"
+	"strings"
+	"time"
 
-	tgbotapi "gopkg.in/telegram-bot-api.v4"
+	"github.com/mmcdole/gofeed"
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-const (
-	BotToken   = "" // your tg token
-	WebhookURL = "" // your ngrok
-)
-
-var rss = map[string]string{
-	"Habr": "https://habr.com/ru/rss/best/daily/?fl=ru",
-}
-
-type RSS struct {
-	Items []Item `xml:"channel>item"`
-}
-
-type Item struct {
-	URL   string `xml:"guid"`
-	Title string `xml:"title"`
-}
-
-func getNews(url string) (*RSS, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	rss := new(RSS)
-	err = xml.Unmarshal(body, rss)
-	if err != nil {
-		return nil, err
-	}
-
-	return rss, nil
-}
+//type Sendable interface {
+//	Send(*Bot, Recipient, *SendOptions) (*Message, error)
+//}
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(BotToken)
+	bot, err := tb.NewBot(tb.Settings{
+		Token:  "YOUR_TOKEN", // telegram bot token 
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
-	// bot.Debug = true
-	fmt.Printf("Authorized on account %s\n", bot.Self.UserName)
+	//b.Handle("Hello!", func(m *tb.Message) {
+	//	b.Send(m.Sender, "Hi!")
+	//})
+	bot.Handle("/getHabr", func(m *tb.Message) {
+		fmt.Println("Loadind...")
+		fp := gofeed.NewParser()
+		feed, _ := fp.ParseURL("https://habr.com/ru/rss/best/daily/?fl=ru")
+		for i, v := range feed.Items {
+			if i < 10 {
+				str := ""
+				strOneNotFullFix, strOneFullFix := "", ""
+				strTwoNotFullFix, strTwoFullFix := "", ""
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook(WebhookURL))
-	if err != nil {
-		panic(err)
-	}
+				bTitle, _ := xml.Marshal(v.Title)
+				strOneNotFullFix += strings.ReplaceAll(string(bTitle), "<string>", "")
+				strOneFullFix += strings.ReplaceAll(strOneNotFullFix, "</string>", "")
+				str += strOneNotFullFix + "\t\t\t\n"
 
-	updates := bot.ListenForWebhook("/")
-
-	go http.ListenAndServe(":80", nil)
-	fmt.Println("Start listen port 80")
-
-	// get updates from channel updates
-	for update := range updates {
-		if url, ok := rss[update.Message.Text]; ok {
-			rss, err := getNews(url)
-			if err != nil {
-				bot.Send(tgbotapi.NewMessage(
-					update.Message.Chat.ID,
-					"Sorry, error =(",
-				))
+				bLink, _ := xml.Marshal(v.Link)
+				strTwoNotFullFix += strings.ReplaceAll(string(bLink), "<string>", "")
+				strTwoFullFix += strings.ReplaceAll(strTwoNotFullFix, "</string>", "")
+				str += strTwoFullFix + "\t\t\t\n"
+				bot.Send(m.Sender, str)
 			}
-			for _, item := range rss.Items {
-				bot.Send(tgbotapi.NewMessage(
-					update.Message.Chat.ID,
-					item.URL+"\n"+item.Title,
-				))
-			}
-		} else {
-			bot.Send(tgbotapi.NewMessage(
-				update.Message.Chat.ID,
-				`Only Habr feed availible`,
-			))
 		}
 
-	}
+		bot.Send(m.Sender, "oho")
+
+		fmt.Println("Success!")
+	})
+	bot.Handle("/getOpenNet", func(m *tb.Message) {
+		fmt.Println("Loadind...")
+		str := make([]string, 20)
+		fp := gofeed.NewParser()
+		feed, err := fp.ParseURL("https://www.opennet.ru/opennews/opennews_all_utf.rss")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for i, v := range feed.Items {
+			if i < 20 {
+				str[i] += fmt.Sprintln(v.Title)
+				str[i] += fmt.Sprintln(v.Link)
+				str[i] += "\t\t\t\n"
+				str[i] += fmt.Sprintln(v.Description)
+				str[i] += "\t\t\t\n\n\n"
+				bot.Send(m.Sender, str[i])
+			}
+		}
+		bot.Send(m.Sender, "oho")
+		fmt.Println("Success!")
+	})
+	bot.Handle(tb.OnText, func(m *tb.Message) {
+		bot.Send(m.Sender, "If you would like to receive the news, \nplease type \"/getOpennet\" or \"/getHabr\"")
+		bot.Send(m.Sender, "Thanks!")
+
+	})
+	bot.Handle("Hi!", func(m *tb.Message) {
+		bot.Send(m.Sender, "Hi!")
+		bot.Send(m.Sender, "If you would like to receive the news, \nplease type \"/getOpennet\" or \"/getHabr\"")
+		bot.Send(m.Sender, "Thanks!")
+	})
+	bot.Handle("What is your name?", func(m *tb.Message) {
+		bot.Send(m.Sender, "T1000")
+	})
+	bot.Handle("stopFeedBot", func(m *tb.Message) {
+		bot.Send(m.Sender, "I am out...")
+		bot.Stop()
+	})
+	bot.Start()
+
 }
