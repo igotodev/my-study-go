958ad0c47bcd86415ef4577aba7b293b7cb66ff4
igotodev <gotodev@protonmail.com>
2021-04-18 14:10:33 +0300

    update

--- FeedMeNewsTg/main.go
+++ FeedMeNewsTg/main.go
@@ -1,6 +1,4 @@
 // FeedMeNews Telegram Bot
-// https://habr.com/ru/rss/best/daily/?fl=ru              - лучшее за день по всем темам
-// https://www.opennet.ru/opennews/opennews_all_utf.rss   - rss от opennet
 
 package main
 
@@ -17,24 +15,49 @@ import (
 //type Sendable interface {
 //	Send(*Bot, Recipient, *SendOptions) (*Message, error)
 //}
+const stopWord = "stopFeedBot" // stop word for the bot
 
 func main() {
 	bot, err := tb.NewBot(tb.Settings{
-		Token:  "YOUR_TOKEN", // telegram bot token
+		Token:  "YOUR_TOKEN", // bot token 
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	//b.Handle("Hello!", func(m *tb.Message) {
-	//b.Send(m.Sender, "Hi!")
+	//	b.Send(m.Sender, "Hi!")
 	//})
-	bot.Handle("/getHabr", func(m *tb.Message) {
+	sayHi := "Hi! " + "\nIf you would like to receive the news,\n" +
+		"please type \"/getSlashdot\", \"/getMediumProg\" or\n\"/getStackOverFlow\"." +
+		" And if you would like to read\n a blog about Go development,\nplease type \"/getEli\"." + "\nThanks!\n"
+	// news sites
+	eli := "https://eli.thegreenplace.net/feeds/all.atom.xml"
+	slashdot := "http://rss.slashdot.org/Slashdot/slashdotMain"
+	medium := "https://medium.com/feed/topic/programming"
+	stackoverflow := "https://stackoverflow.blog/feed/"
+
+	readRSS(slashdot, "/getSlashdot", bot)
+	readRSS(medium, "/getMediumProg", bot)
+	readRSS(stackoverflow, "/getStackOverFlow", bot)
+	readXML(eli, "/getEli", bot)
+
+	massage("What is your name?", "T1000", bot)
+	massage(tb.OnText, sayHi, bot)
+
+	bot.Handle(stopWord, func(m *tb.Message) {
+		bot.Send(m.Sender, "I am out...")
+		bot.Stop()
+	})
+	bot.Start()
+}
+// xml reading and delivery function 
+func readXML(address string, command string, bot *tb.Bot) {
+	bot.Handle(command, func(m *tb.Message) {
 		fmt.Println("Loadind...")
 		fp := gofeed.NewParser()
 
-		feed, _ := fp.ParseURL("https://habr.com/ru/rss/best/daily/?fl=ru")
+		feed, _ := fp.ParseURL(address)
 		for i, v := range feed.Items {
 			if i < 10 {
 				str := ""
@@ -45,6 +68,8 @@ func main() {
 				strOneFullFix += strings.ReplaceAll(strOneNotFullFix, "</string>", "")
 				str += strOneNotFullFix + "\t\t\t\n"
 
+				str += v.Published + "\t\t\t\n"
+
 				strTwoNotFullFix += strings.ReplaceAll(v.Link, "<string>", "")
 				strTwoFullFix += strings.ReplaceAll(strTwoNotFullFix, "</string>", "")
 				str += strTwoFullFix + "\t\t\t\n"
@@ -52,49 +77,35 @@ func main() {
 				bot.Send(m.Sender, str)
 			}
 		}
-
 		bot.Send(m.Sender, "oho")
 
 		fmt.Println("Success!")
 	})
-	bot.Handle("/getOpenNet", func(m *tb.Message) {
+}
+// rss reading and delivery function 
+func readRSS(address string, command string, bot *tb.Bot) {
+	bot.Handle(command, func(m *tb.Message) {
 		fmt.Println("Loadind...")
-		str := make([]string, 20)
+		str := make([]string, 10)
 		fp := gofeed.NewParser()
-		feed, err := fp.ParseURL("https://www.opennet.ru/opennews/opennews_all_utf.rss")
+		feed, err := fp.ParseURL(address)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		for i, v := range feed.Items {
-			if i < 20 {
+			if i < 10 {
 				str[i] += fmt.Sprintln(v.Title)
 				str[i] += fmt.Sprintln(v.Link)
-				str[i] += "\t\t\t\n"
-				str[i] += fmt.Sprintln(v.Description)
-				str[i] += "\t\t\t\n\n\n"
 				bot.Send(m.Sender, str[i])
 			}
 		}
 		bot.Send(m.Sender, "oho")
 		fmt.Println("Success!")
 	})
-	bot.Handle(tb.OnText, func(m *tb.Message) {
-		bot.Send(m.Sender, "If you would like to receive the news, \nplease type \"/getOpenNet\" or \"/getHabr\"")
-		bot.Send(m.Sender, "Thanks!")
-
+}
+// for message delivery
+func massage(userTxt string, botTxt string, bot *tb.Bot) {
+	bot.Handle(userTxt, func(m *tb.Message) {
+		bot.Send(m.Sender, botTxt)
 	})
-	bot.Handle("Hi!", func(m *tb.Message) {
-		bot.Send(m.Sender, "Hi!")
-		bot.Send(m.Sender, "If you would like to receive the news, \nplease type \"/getOpenNet\" or \"/getHabr\"")
-		bot.Send(m.Sender, "Thanks!")
-	})
-	bot.Handle("What is your name?", func(m *tb.Message) {
-		bot.Send(m.Sender, "T1000")
-	})
-	bot.Handle("stopFeedBot", func(m *tb.Message) {
-		bot.Send(m.Sender, "I am out...")
-		bot.Stop()
-	})
-	bot.Start()
-
 }
